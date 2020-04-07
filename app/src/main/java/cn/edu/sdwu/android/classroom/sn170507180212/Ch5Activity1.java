90aa1bb82650144de0c694b24c7cc4370ba52d40
QL805 <805008123@qq.com>
2020-04-07 21:15:57 +0800

    class

--- app/src/main/java/cn/edu/sdwu/android/classroom/sn170507180212/Ch5Activity1.java
+++ app/src/main/java/cn/edu/sdwu/android/classroom/sn170507180212/Ch5Activity1.java
@@ -1,7 +1,14 @@
 package cn.edu.sdwu.android.classroom.sn170507180212;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -40,5 +47,54 @@ public class Ch5Activity1 extends AppCompatActivity {
         toast.setDuration(Toast.LENGTH_SHORT);
         toast.setGravity(Gravity.CENTER,0,0);
         toast.show();
+    }
+
+    public void notification(View view){
+        Notification.Builder builder=new Notification.Builder(this);
+        //在构造器中，完成通知各种属性的设置
+        builder.setContentTitle("title");
+        builder.setContentText("message");
+        builder.setWhen(System.currentTimeMillis());
+        builder.setSmallIcon(android.R.drawable.ic_dialog_email);//引用系统资源
+        Notification notification=builder.build();
+        //发送通知
+        NotificationManager notificationManager= (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(101,notification);
+
+    }
+
+    public void cancel_notification(View view){
+        //使用NotificationManager 发送通知
+        NotificationManager notificationManager=(NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.cancel(101);
+    }
+    public void alter_dialog(View view){
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        //在构造器中设置对话框的属性
+        builder.setTitle("title");
+        builder.setMessage("message");
+        builder.setIcon(android.R.drawable.ic_dialog_email);
+        builder.setPositiveButton("confirm", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(Ch5Activity1.this,"confirm",Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setPositiveButton("exit", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(Ch5Activity1.this,"exit",Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setPositiveButton("cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //添加点击按键后的处理
+                Toast.makeText(Ch5Activity1.this,"confirm",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.create().show();
+
     }
 }
---
+++ app/src/main/java/cn/edu/sdwu/android/classroom/sn170507180212/Ch6Activity1.java
@@ -0,0 +1,23 @@
+package cn.edu.sdwu.android.classroom.sn170507180212;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class Ch6Activity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ch6_1);
+        //在Java中获取
+        String content=getString(R.string.hello);
+
+        Log.i(Ch6Activity1.this.toString(),content);
+
+        String sms=getString(R.string.sms);
+        String.format(sms,100,"张三");
+        Log.i(Ch6Activity1.this.toString(),sms);
+
+    }
+}
