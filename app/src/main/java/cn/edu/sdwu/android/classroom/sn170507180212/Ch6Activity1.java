9d59a2592a21b525fdeab829b65bfeefcee3d160
QL805 <805008123@qq.com>
2020-04-07 21:34:51 +0800

    class

--- app/src/main/java/cn/edu/sdwu/android/classroom/sn170507180212/Ch6Activity1.java
+++ app/src/main/java/cn/edu/sdwu/android/classroom/sn170507180212/Ch6Activity1.java
@@ -16,8 +16,8 @@ public class Ch6Activity1 extends AppCompatActivity {
         Log.i(Ch6Activity1.this.toString(),content);
 
         String sms=getString(R.string.sms);
-        String.format(sms,100,"张三");
-        Log.i(Ch6Activity1.this.toString(),sms);
+        String s=String.format(sms,100,"张三");
+        Log.i(Ch6Activity1.this.toString(),s);
 
     }
 }
